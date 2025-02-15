1e5e2d49285f0f7e2a6554f380bda2daa2b6df08
Shant Dashjian <shantgdashjian@gmail.com>
2025-02-15 23:55:22 +0400

    Get the db url from process.env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 
-import { config } from "./src/config";
-
 export default defineConfig({
   out: "./src/db/migrations",
   schema: "./src/db/schema.ts",
   dialect: "turso",
   dbCredentials: {
-    url: config.db.url || "",
+    url: process.env.DATABASE_URL || "",
   },
 });
